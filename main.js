2a70f96f1aa7320ed55499e1dc268f47dd0f8bdd
Lucas Roteño <lucasrot1@hotmail.com>
2023-03-07 05:16:07 -0300

    Modificando form

--- main.js
+++ main.js
@@ -79,9 +79,23 @@ sections.forEach(section => {
     observer.observe(section);
 });
 
+// --- formulario ---
+const $form = document.querySelector('#form')
+        
+$form.addEventListener('submit', handleSubmit)
 
-
-const $form = document.querySelector("#form")
-
-
-$form.addEventListener("submit", handleSubmit)
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) { 
+    this.reset()
+    alert('Gracias por contactarnos, te escribiremos pronto')
+    }
+}
